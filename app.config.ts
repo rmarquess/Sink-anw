5e10d205a282c7f990cfb8d66536bee48253822e
Rafael Marques <rmarquess@hotmail.com>
2024-09-14 13:38:42 -0300

    Update app.config.ts
    Update title and description

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
-  title: 'Sink',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  title: 'AprendaNaWeb',
+  description: 'Um Simples / Rápido / Seguro Link Shortener com Analytics, 100% rodando na Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
